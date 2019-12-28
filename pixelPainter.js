8fb0870ddb09562905c8fdd8a0e6f91415ac4e36
benjorama <benmacomb@gmail.com>
2019-12-27 22:36:54 -0700

    Update pixelPainter.js to generate square grid.

--- pixelPainter.js
+++ pixelPainter.js
@@ -1,12 +1,12 @@
 const DEFAULT_GRID_SIZE = 16;
 const GRID_CELL_SIZE = "20px";
 
-function createGridContainer( width ) {
+function createGridContainer( size ) {
   const gridContainer = document.createElement( "div" );
   gridContainer.setAttribute( "class", "gridContainer" );
   gridContainer.style.display = "grid";
   let gridTemplateColumns = "";
-  for ( let i = 0; i < width; i++ ) {
+  for ( let i = 0; i < size; i++ ) {
     gridTemplateColumns += GRID_CELL_SIZE + " ";
   }
   gridContainer.style.gridTemplateColumns = gridTemplateColumns;
@@ -24,11 +24,11 @@ function createGridCell() {
   return gridCell;
 }
 
-function createGrid( width, height ) {
+function createGrid( size ) {
   if ( !document.querySelector( ".gridContainer" ) ) {
-    const gridContainer = createGridContainer( width );
-    for ( let i = 0; i < width; i++ ) {
-      for ( let j = 0; j < height; j++ ) {
+    const gridContainer = createGridContainer( size );
+    for ( let i = 0; i < size; i++ ) {
+      for ( let j = 0; j < size; j++ ) {
         const gridCell = createGridCell();
         gridContainer.appendChild( gridCell );
       }
@@ -37,31 +37,28 @@ function createGrid( width, height ) {
   } else {
     const gridContainer = document.querySelector( ".gridContainer" );
     gridContainer.remove();
-    createGrid( width, height );    
+    createGrid( size );    
   }
 }
 
 function populateDropDownSize() {
-  const dropDownSelector = document.querySelectorAll( ".canvasSize select" );
+  const dropDownSelector = document.querySelector( ".canvasSize select" );
   for ( let i = 1; i <= 128; i++ ) {
-    dropDownSelector.forEach( element => {
-      const option = document.createElement( "option" );
-      option.value = i;
-      option.text = i;
-      if ( option.value == DEFAULT_GRID_SIZE ) {
-        option.setAttribute( "selected", "selected" );
-      }
-      element.appendChild( option );
-    });
+    const option = document.createElement( "option" );
+    option.value = i;
+    option.text = i;
+    if ( option.value == DEFAULT_GRID_SIZE ) {
+      option.setAttribute( "selected", "selected" );
+    }
+    dropDownSelector.appendChild( option );
   }
 }
 
 populateDropDownSize();
 const createCanvasButton = document.querySelector( ".canvasSize button" );
 createCanvasButton.addEventListener( "click", () => {
-  const width = document.querySelector( ".canvasSize #width" ).value;
-  const height = document.querySelector( ".canvasSize #height" ).value;
-  createGrid( width, height );
+  const size = document.querySelector( ".canvasSize select" ).value;
+  createGrid( size );
 });
 
 
